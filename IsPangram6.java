c973c16b817735b4ff34effdc1e33e67778d9d29
Charitha <151384903+charitha-26@users.noreply.github.com>
2024-09-29 19:41:36 +0530

    Update and rename IsPangram.java to IsPangram6.java

--- IsPangram6.java
+++ IsPangram6.java
@@ -1,6 +1,6 @@
 //6.	Write a java program to find whether given sentence is Pangram or not. If it is pangram, then print 1 else print -1.A pangram is a sentence containing every letter in the English Alphabet. Ex: The quick brown fox jumps over the lazy Dog.
 import java.util.*;
-public class IsPangram {
+public class IsPangram6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
