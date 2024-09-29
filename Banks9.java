36a9f2c83a39d1821915362fc494ada42cf3dbf8
Charitha <151384903+charitha-26@users.noreply.github.com>
2024-09-29 19:46:02 +0530

    Create Banks9.java

---
+++ Banks9.java
@@ -0,0 +1,40 @@
+//9.	Create an abstract class 'Bank' with an abstract method 'getBalance'. $100, $150 and $200 are deposited in banks A, B and C respectively. 'BankA', 'BankB' and  'BankC' are subclasses of class 'Bank', each having a method named 'getBalance'.
+class Bank{
+	 int getBalance() {
+		 return 0;
+	 }
+}
+
+class BankA extends Bank{
+	int getBalance(){
+		return 100;
+	}
+}
+
+class BankB extends Bank{
+	int getBalance(){
+		return 150;
+	}
+}
+
+class BankC extends Bank{
+	int getBalance(){
+		return 200;
+	}
+}
+
+public class Banks9 {
+
+	public static void main(String[] args) {
+		BankA bankA = new BankA();
+		System.out.println("Amount deposited in Bank A is $"+ bankA.getBalance());
+		
+		BankB bankB = new BankB();
+		System.out.println("Amount deposited in Bank B is $"+ bankB.getBalance());
+		
+		BankC bankC = new BankC();
+		System.out.println("Amount deposited in Bank C is $"+ bankC.getBalance());
+		
+	}
+
+}
