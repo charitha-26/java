5ff57742abd1843bc16936cd250945afb68ae344
Charitha <151384903+charitha-26@users.noreply.github.com>
2024-09-29 19:40:57 +0530

    Create IsPangram.java

---
+++ IsPangram.java
@@ -0,0 +1,39 @@
+//6.	Write a java program to find whether given sentence is Pangram or not. If it is pangram, then print 1 else print -1.A pangram is a sentence containing every letter in the English Alphabet. Ex: The quick brown fox jumps over the lazy Dog.
+import java.util.*;
+public class IsPangram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean[]  alphabets = new boolean[26];
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the sentence: ");
+		String sentence = sc.nextLine();
+		sentence = sentence.toLowerCase();
+		
+		for(int idx = 0; idx < sentence.length();idx++) {
+			char chr = sentence.charAt(idx);
+			if(chr >= 'a' && chr <= 'z'){
+					alphabets[chr - 'a'] = true;
+			}
+		}
+		
+		boolean isPangram = true;
+		
+		for(int idx = 0; idx < 26; idx++) {
+			if(!alphabets[idx]) {
+				isPangram = false;
+				break;
+			}
+		}
+		
+		if(isPangram) {
+			System.out.println("Pangram");
+		}
+		else {
+			System.out.println("Not a Pangram");
+		}
+
+	}
+
+}
